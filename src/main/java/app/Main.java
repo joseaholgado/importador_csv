4e86a5ba1f50dadadf48cd551339a8a767681755
Jose Antonio <joseantonio-hb@hotmail.com>
2025-10-30 22:44:39 +0100

    -Update- Añado las excepciones del archivo csv tanto en el Main y en la clase Menu

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,6 +3,8 @@ import clases.Order;
 import clases.OrderImporter;
 import clases.Menu;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
 import java.util.List;
 
@@ -10,22 +12,63 @@ import java.util.List;
 public class Main {
     /**
      * Programa principal que:
-     * - Lee un archivo CSV con datos de ventas
+     * - Lee un archivo CSV con datos
      * - Convierte cada registro en un objeto Order
      * - Muestra un menú interactivo en la consola para visualizar y ordenar los pedidos
+     * - Tiene en cuenta que el usuario elija solo csv y si elige otro que no es
      */
 
     public static void main(String[] args) throws Exception {
+        String newRute = null;
+        File selectedFile = null;
+        boolean validFile=false;
 
-        String file = "src/main/resources/RegistroVentas1.csv";
+
+        do {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Selecciona el archivo CSV");
+
+            //Es lo que limita para ficheros csv y evitar ficheros maliciosos
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos CSV (*.csv)", "csv");
+            fileChooser.setFileFilter(filter);
+
+            int userSelection = fileChooser.showOpenDialog(null);
+
+                if (userSelection == JFileChooser.APPROVE_OPTION) {
+                    selectedFile = fileChooser.getSelectedFile();
+
+                    // Verificación adicional por seguridad (por si el usuario escribe manualmente otro tipo)
+                    if (!selectedFile.getName().toLowerCase().endsWith(".csv")) {
+                        System.out.println("Solo se permiten archivos con extensión .csv");
+                        System.out.println("\nVuelva a intertar con archivo CSV");
+                    } else {
+                        validFile = true;
+                    }
+                } else {
+                    System.out.println("No se seleccionó ningún archivo. Programa cancelado.");
+                    return;
+                }
+        } while (!validFile);
+
+        String filePath = selectedFile.getAbsolutePath();
+        System.out.println("Has seleccionado el archivo " + selectedFile.getName() + "\n");
 
         OrderImporter importer = new OrderImporter();
-        List<Order> orders = importer.importCSV(file);
+        List<Order> orders = importer.importCSV(filePath);
 
-        Menu menu = new Menu(orders);
-        menu.mostrarMenu();
+        do {
+            Menu menu = new Menu(orders, importer);
+            newRute = menu.showMenu();
 
-        //System.out.println("\nÉxito");
+            if (newRute != null) {
+                selectedFile = new File(newRute);
+                System.out.println("Cargando nuevo archivo " + selectedFile.getName());
+                orders = importer.importCSV(newRute);
+            }
+        } while (newRute != null);
 
+        System.out.println("\nArchivo cargado correctamente: " + filePath);
     }
 }
+
+
--- src/main/java/clases/Menu.java
+++ src/main/java/clases/Menu.java
@@ -1,5 +1,8 @@
 package clases;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 import java.util.Comparator;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -17,63 +20,111 @@ import java.util.Scanner;
 
 public class Menu {
     private List<Order> orders;
+    private OrderImporter importer;
+    private Boolean salir = false;
 
-    public Menu(List<Order> orders) {
+
+    public Menu(List<Order> orders, OrderImporter importer) {
         this.orders = orders;
+        this.importer = importer;
     }
 
-    public void mostrarMenu() {
+    public String showMenu() {
         int opcion = 0;
         Scanner sc = new Scanner(System.in);
+        String newRute = null;
 
 
         do {
             System.out.println("\n--- MENÚ ---");
             System.out.println("1. Mostrar todos los pedidos");
             System.out.println("2. Ordenar por OrderID");
-            System.out.println("3. Salir");
+            System.out.println("3. Cambiar archivo CSV");
+            System.out.println("4. Salir");
             System.out.print("Elige una opción: ");
 
             try {
                 opcion = sc.nextInt();  // intenta leer un número
 
 
-            switch(opcion) {
-                case 1:
-                    mostrarPedidos();
-                    break;
-                case 2:
-                    ordenarPorOrderId();
-                    break;
-                case 3:
-                    System.out.println("Saliendo...");
-                    break;
-                default:
-                    System.out.println("Opción no válida");
-            }
+                switch (opcion) {
+                    case 1:
+                        showOrders();
+                        break;
+                    case 2:
+                        sortByOrderId();
+                        break;
+                    case 3:
+                        newRute = changeFile();
+                        if (newRute != null) return newRute;
+                        break;
+                    case 4:
+                        System.out.println("Saliendo...");
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Opción no válida");
+                }
             } catch (InputMismatchException e) {
                 System.out.println("Error: Debes ingresar un número (no letras).");
                 sc.nextLine();
             }
-            
-        } while(opcion != 3);
 
+        } while (opcion != 4);
+
+        return null;
     }
 
 
-    private void mostrarPedidos() {
+    private void showOrders() {
         orders.sort(Comparator.comparing(Order::getId));
-        for(Order o : orders) {
+        for (Order o : orders) {
             System.out.println(o);
         }
     }
 
-    private void ordenarPorOrderId() {
+    private void sortByOrderId() {
         orders.sort(Comparator.comparing(Order::getOrderId, Comparator.nullsLast(String::compareTo)));
         System.out.println("Pedidos ordenados por OrderID");
-        for(Order o : orders) {
-            System.out.println(o);
+        for (Order order : orders) {
+            System.out.println(order);
         }
     }
 
+    private String changeFile() {
+        Boolean validFile = false;
+        File selectedFile = null;
+
+        do {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Selecciona el nuevo archivo CSV");
+
+            //Es lo que limita para ficheros csv y evitar ficheros maliciosos
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos CSV (*.csv)", "csv");
+            fileChooser.setFileFilter(filter);
+
+            int userSelection = fileChooser.showOpenDialog(null);
+
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                selectedFile = fileChooser.getSelectedFile();
+
+                // Verificación adicional por seguridad (por si el usuario escribe manualmente otro tipo)
+                if (!selectedFile.getName().toLowerCase().endsWith(".csv")) {
+                    System.out.println("Solo se permiten archivos con extensión .csv");
+                } else {
+                    validFile = true;
+                    System.out.println("Has seleccionado el archivo " + selectedFile.getName() + "\n");
+                }
+            } else {
+                System.out.println("No se seleccionó ningún archivo.");
+                return null;
+            }
+
+        } while (!validFile);
+
+        return selectedFile.getAbsolutePath();
+    }
 }
+
+
+
